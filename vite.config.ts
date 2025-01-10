1bc0ba2789bd5923f6ff848deea45b25366a17b1
pez3002 <ametatheton2@gmail.com>
2025-01-10 03:01:48 -0800

    Github Pages Deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
+import { ghPages } from "vite-plugin-gh-pages";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  publicDir: "public", // Add this line to include the assets directory
+  plugins: [react(), ghPages()],
+  base: "/joelewaldo/", // Replace <repository-name> with your GitHub repo name
 });
